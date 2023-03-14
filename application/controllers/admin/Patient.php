b7d7c979c8658654efbd0c4c144bcb600fc4fa5d
Thakur Prasad <thakurprasadlhs@gmail.com>
2023-03-15 00:28:08 +0530

    herader allow

--- application/controllers/admin/Patient.php
+++ application/controllers/admin/Patient.php
@@ -8,6 +8,10 @@ class patient extends Admin_Controller
 
     public function __construct()
     {
+        Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+        Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+          Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
+
         parent::__construct();
         $this->config->load("payroll");
         $this->config->load("image_valid");
